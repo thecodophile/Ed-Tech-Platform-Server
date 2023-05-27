f33322daf364f0f1aee411c929cc536b11c48c26
Somnath Dey <sdey.aklakhi@gmail.com>
2023-05-27 15:42:55 +0530

    Add RatingAndReview and getCourseDetails controller
    all RatingAndReview controllers are usefull to handle rating and reviews, getCourseDetails controller inside CourseController.js use to fetch all data of a particular course

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -125,7 +125,7 @@ exports.createCourse = async (req, res) => {
 };
 
 //getAllCourses
-exports.showAllCourses = async (req, res) => {
+exports.getAllCourses = async (req, res) => {
   try {
     const allCourses = await Course.find(
       {},
@@ -152,6 +152,51 @@ exports.showAllCourses = async (req, res) => {
       success: false,
       message: "Cannot fetch course data",
       error: error.message,
+    });
+  }
+};
+
+//getCourseDetails
+exports.getCourseDetails = async (req, res) => {
+  try {
+    //get id
+    const { courseId } = req.body;
+    //find course details
+    const courseDetails = await Course.find({ _id: courseId })
+      .populate({
+        path: "instructor",
+        populate: {
+          path: "additionalDetails",
+        },
+      })
+      .populate("category")
+      .populate("ratingAndreviews")
+      .populate({
+        path: "courseContent",
+        populate: {
+          path: "subSection",
+        },
+      })
+      .exec();
+
+    //validation
+    if (!courseDetails) {
+      return res.status(400).json({
+        success: false,
+        message: `Could not find the course with ${courseId}`,
+      });
+    }
+    //return response
+    return res.status(200).json({
+      success: true,
+      message: "Course Details fetched successfully",
+      data: courseDetails,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
     });
   }
 };
